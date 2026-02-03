06342269754b0cde761e819f43a1e07bb95b2b6d
tarun <aju55355@gmail.com>
2026-02-03 13:00:15 +0530

    fonrnt-

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -293,7 +293,7 @@ function closePopup(){
             let percentText = '';
 
             if (minPercent > 0 && maxPercent > 0) {
-                percentText = `Minimum Sharing ${minPercent}% - Maximum Sharing ${maxPercent}%`;
+                percentText = `Min : Sharing ${minPercent}% - Max : Sharing ${maxPercent}%`;
             }
             else if (minPercent > 0) {
                 percentText = `Minimum Sharing ${minPercent}%`;
