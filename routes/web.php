2e10a6aa00f3b2d78f97f863642734a3b5bf7899
tarun <aju55355@gmail.com>
2026-02-03 17:29:35 +0530

    fix b2b

--- routes/web.php
+++ routes/web.php
@@ -4,40 +4,59 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\Admin\AuthController;
 
-// Front Page
+/*
+|--------------------------------------------------------------------------
+| FRONTEND ROUTES (PUBLIC)
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', [SiteController::class, 'front'])->name('front');
+
 Route::get('/b2b-sites',[SiteController::class,'b2b']);
 Route::get('/b2c-sites',[SiteController::class,'b2c']);
 
+/*
+|--------------------------------------------------------------------------
+| ADMIN AUTH
+|--------------------------------------------------------------------------
+*/
 
-// Admin Authentication Routes
 Route::prefix('admin')->group(function () {
+
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->name('admin.logout');
+
 });
 
-// Protected Admin Routes
+/*
+|--------------------------------------------------------------------------
+| ADMIN PANEL (PROTECTED)
+|--------------------------------------------------------------------------
+*/
+
 Route::middleware(['admin.auth'])->prefix('admin')->group(function () {
+
     Route::get('/', [SiteController::class, 'admin'])->name('admin');
+
     Route::post('/store', [SiteController::class, 'store'])->name('sites.store');
     Route::get('/edit/{id}', [SiteController::class, 'edit'])->name('sites.edit');
     Route::post('/update/{id}', [SiteController::class, 'update'])->name('sites.update');
     Route::delete('/delete/{id}', [SiteController::class, 'delete'])->name('sites.delete');
 
+    /* CATEGORY */
 
-    // routes/web.php
-    Route::get('/admin/categories', [SiteController::class, 'categories'])->name('categories');
-    Route::post('/admin/category/update', [SiteController::class, 'updateCategory'])->name('category.update');
-    Route::post('/admin/category/delete', [SiteController::class, 'deleteCategory'])->name('category.delete');
-    Route::post('/category/store', [SiteController::class, 'storeCategory'])
-        ->name('category.store');
+    Route::get('/categories', [SiteController::class, 'categories'])->name('categories');
+    Route::post('/category/update', [SiteController::class, 'updateCategory'])->name('category.update');
+    Route::post('/category/delete', [SiteController::class, 'deleteCategory'])->name('category.delete');
+    Route::post('/category/store', [SiteController::class, 'storeCategory'])->name('category.store');
 
-    Route::post('/admin/sites/reorder', [SiteController::class, 'reorder'])
-        ->name('sites.reorder');
+    /* REORDER */
 
-        Route::post('/sites/bulk-type',
- [SiteController::class,'bulkType'])
- ->name('sites.bulk.type');
+    Route::post('/sites/reorder', [SiteController::class, 'reorder'])->name('sites.reorder');
+
+    /* BULK B2B / B2C */
+
+    Route::post('/sites/bulk-type',[SiteController::class,'bulkType'])->name('sites.bulk.type');
 
 });
